9d7b3a8bdee9ceb30eb87e9c6e9610dcc697e3bb
YourfriendRND <kupeirc@gmail.com>
2023-05-09 13:58:02 +0300

    Добавил парсинг комманд из директории cli-command

--- src/app/cli.ts
+++ src/app/cli.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+import path from 'node:path';
 import { CliCommandInterface } from '../core/cli-command/cli-command.interface';
 
 type ParsedCommand = Record<string, string[]>;
@@ -5,8 +7,19 @@ type ParsedCommand = Record<string, string[]>;
 export default class CliCommandManager {
   private commands: {[commandName: string]: CliCommandInterface} = {};
   private defaultCommand = '--help';
+  private commandDirPath = './src/core/cli-command';
 
-  public registerCommands = (commandList: CliCommandInterface[]): void => {
+  public parseCommandFiles = async (): Promise<CliCommandInterface[]> => {
+    const commandFiles = fs.readdirSync(path.resolve(this.commandDirPath))
+      .filter((file) => (file.includes('.command.ts') || file.includes('.command.js')) && !file.includes('.js.map'));
+    const commandInstances: CliCommandInterface[] = await Promise.all(commandFiles.map(async (file) => {
+      const module = await import(`../core/cli-command/${file}`);
+      return new module.default();
+    }));
+    return commandInstances;
+  };
+
+  public registerCommands = async (commandList: CliCommandInterface[]): Promise<void> => {
     commandList.reduce((savedList, commandItem) => {
       savedList[commandItem.name] = commandItem;
       return savedList;
--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
 import CliCommandManager from './app/cli.js';
-import HelpCommand from './core/cli-command/help.command.js';
-import VersionCommand from './core/cli-command/version.command.js';
-import ImportCommand from './core/cli-command/import.command.js';
+// import HelpCommand from './core/cli-command/help.command.js';
+// import VersionCommand from './core/cli-command/version.command.js';
+// import ImportCommand from './core/cli-command/import.command.js';
 
 
 const commandManager = new CliCommandManager();
-commandManager.registerCommands([
-  new HelpCommand(), new VersionCommand(), new ImportCommand()
-]);
 
-commandManager.processedCommand(process.argv);
+commandManager.parseCommandFiles().then((commands) => {
+  commandManager.registerCommands(commands);
+  commandManager.processedCommand(process.argv);
+});
+
+// commandManager.registerCommands([
+//   new HelpCommand(), new VersionCommand(), new ImportCommand()
+// ]);
+
+//commandManager.processedCommand(process.argv);
