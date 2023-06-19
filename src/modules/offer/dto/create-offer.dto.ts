6d0b29ddfe89b44a8ee364b205d476f22defb0d6
kupeirc@gmail.com <aivanov.reon@gmail.com>
2023-06-19 18:26:02 +0300

    Продолжение валидации создания предложения

--- src/modules/offer/dto/create-offer.dto.ts
+++ src/modules/offer/dto/create-offer.dto.ts
@@ -1,6 +1,21 @@
 import { OfferCoordinates } from '../../../types/offer.type';
-import { IsDateString, MinLength, MaxLength, isIn, IsEnum } from 'class-validator';
-import { OfferSchemaLimits, CITIES } from '../offer.constants.js';
+import {
+  IsDateString,
+  MinLength,
+  MaxLength,
+  IsEnum,
+  IsArray,
+  IsUrl,
+  ArrayNotEmpty,
+  ArrayMinSize,
+  ArrayMaxSize,
+  IsBoolean,
+  IsDecimal,
+  Min,
+  Max,
+  IsInt
+} from 'class-validator';
+import { OfferSchemaLimits, CITIES, HOUSING_TYPES } from '../offer.constants.js';
 import { offerValidateErrorMessage } from './offer-validate-error-message.js';
 
 export default class CreateOfferDto {
@@ -19,16 +34,46 @@ export default class CreateOfferDto {
   @IsEnum(CITIES, {message: offerValidateErrorMessage.city.message })
   public city!: string;
 
+  @IsUrl({}, {message: offerValidateErrorMessage.prevImageUrl.message })
   public prevImageUrl!: string;
+
+  @IsArray({message: offerValidateErrorMessage.photos.notArrayMessage})
+  @ArrayNotEmpty({message: offerValidateErrorMessage.photos.emptyArrayMessage })
+  @ArrayMinSize(OfferSchemaLimits.REQUIRED_PHOTOS_COUNT, {message: offerValidateErrorMessage.photos.minArrayLengthMessage })
+  @ArrayMaxSize(OfferSchemaLimits.REQUIRED_PHOTOS_COUNT, {message: offerValidateErrorMessage.photos.maxArrayLengthMessage })
   public photos!: string[];
+
+  @IsBoolean({ message: offerValidateErrorMessage.isPremium.message })
   public isPremium!: boolean;
+
+  @IsDecimal({ 'decimal_digits': '1'}, {message: offerValidateErrorMessage.rating.decimalMessage })
+  @Min(OfferSchemaLimits.MIN_OFFER_RATING, {message: offerValidateErrorMessage.rating.minRatingMessage })
+  @Max(OfferSchemaLimits.MAX_OFFER_RATING, {message: offerValidateErrorMessage.rating.maxRatingMessage })
   public rating!: number;
+
+  @IsEnum(HOUSING_TYPES, {message: offerValidateErrorMessage.housingType.message })
   public housingType!: string;
+
+  @IsInt({message: offerValidateErrorMessage.roomCount.intMessage })
+  @Min(OfferSchemaLimits.MIN_ROOM_COUNT, {message: offerValidateErrorMessage.roomCount.minRoomCountMessage })
+  @Max(OfferSchemaLimits.MAX_ROOM_COUNT, {message: offerValidateErrorMessage.roomCount.maxRoomCountMessage })
   public roomCount!: number;
+
+  @IsInt({message: offerValidateErrorMessage.guestCount.message })
+  @Min(OfferSchemaLimits.MIN_GUEST_COUNT, {message: offerValidateErrorMessage.guestCount.minGuestCountMessage })
+  @Max(OfferSchemaLimits.MAX_GUEST_COUNT, {message: offerValidateErrorMessage.guestCount.maxGuestCountMessage })
   public guestCount!: number;
+
+  @IsInt({message: offerValidateErrorMessage.price.intMessage })
+  @Min(OfferSchemaLimits.MIN_OFFER_PRICE, {message: offerValidateErrorMessage.price.minPriceMessage })
+  @Max(OfferSchemaLimits.MAX_OFFER_PRICE, {message: offerValidateErrorMessage.price.maxPriceMessage })
   public price!: number;
+
   public facilities!: string[];
+
   public authorId!: string;
+
   public commentCount!: number;
+
   public coordinates!: OfferCoordinates;
 }
--- src/modules/offer/dto/offer-validate-error-message.ts
+++ src/modules/offer/dto/offer-validate-error-message.ts
@@ -1,4 +1,4 @@
-import { CITIES, OfferSchemaLimits } from '../offer.constants.js';
+import { CITIES, HOUSING_TYPES, OfferSchemaLimits } from '../offer.constants.js';
 
 export const offerValidateErrorMessage = {
   name: {
@@ -17,6 +17,48 @@ export const offerValidateErrorMessage = {
 
   city: {
     message: `City should be item of list: ${CITIES.join(', ')}`
-  }
+  },
 
+  prevImageUrl: {
+    message: 'Preview image is not match url type of string'
+  },
+
+  photos: {
+    notArrayMessage: 'Photos field should be array',
+    emptyArrayMessage: 'Array of photos should not be empty',
+    minArrayLengthMessage: `Length of photos array should be ${OfferSchemaLimits.REQUIRED_PHOTOS_COUNT}`,
+    maxArrayLengthMessage: `Length of photos array should be ${OfferSchemaLimits.REQUIRED_PHOTOS_COUNT}`
+  },
+
+  isPremium: {
+    message: 'isPremium should be boolean type'
+  },
+
+  rating: {
+    decimalMessage: 'Offer rating should be integer or decimal number with one sign after decimal',
+    minRatingMessage: `Minimum rating offer number should be ${OfferSchemaLimits.MIN_OFFER_RATING}`,
+    maxRatingMessage: `Maximum rating offer number should be ${OfferSchemaLimits.MAX_OFFER_RATING}`
+  },
+
+  housingType: {
+    message: `The housing type should be one item of the list: ${HOUSING_TYPES.join(', ')}`,
+  },
+
+  roomCount: {
+    intMessage: 'Offer room count should be contain integer value',
+    minRoomCountMessage: `Offer room count minimum should be ${OfferSchemaLimits.MIN_ROOM_COUNT}`,
+    maxRoomCountMessage: `Offer room count maximum should be ${OfferSchemaLimits.MAX_ROOM_COUNT}`
+  },
+
+  guestCount: {
+    message: 'Offer guest count should be contain integer value',
+    minGuestCountMessage: `Offer guest count minimum should be ${OfferSchemaLimits.MIN_GUEST_COUNT}`,
+    maxGuestCountMessage: `Offer guest count maximum should be ${OfferSchemaLimits.MAX_GUEST_COUNT}`
+  },
+
+  price: {
+    intMessage: 'Offer price should be integer value',
+    minPriceMessage: `Minimum offer price should be ${OfferSchemaLimits.MIN_OFFER_PRICE}`,
+    maxPriceMessage: `Maximum offer price should be ${OfferSchemaLimits.MAX_OFFER_PRICE}`
+  }
 };
